bfb72b3d5e26e5f723a01784fb0071eaeff96673
yliang18 <yliang18@student.ubc.ca>
2024-01-29 21:34:57 -0800

    added writefile,readfile as public in InsightFacade.ts

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -8,6 +8,8 @@ import {
 	ResultTooLargeError
 } from "./IInsightFacade";
 import DatasetProcessor from "./DatasetProcessor";
+import * as fs from "fs-extra";
+import JSZip from "jszip";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -17,6 +19,7 @@ import DatasetProcessor from "./DatasetProcessor";
 export default class InsightFacade implements IInsightFacade {
 	private datasetProcessor: DatasetProcessor;
 	private datasets: Map<string, any[]>;
+
 	constructor() {
 		console.log("InsightFacadeImpl::init()");
 		this.datasetProcessor = new DatasetProcessor();
@@ -26,7 +29,7 @@ export default class InsightFacade implements IInsightFacade {
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
 		return new Promise<string[]>(() => {
 			const dp = new DatasetProcessor();
-			if (id == null || id === "" || id.trim().length === 0) {
+			if (id === null || id === "" || id.trim().length === 0 || id.includes("_") || id.includes(" ")) {
 				return Promise.reject(new InsightError("id is null or empty"));
 			}
 			if (this.datasets.has(id)) {
@@ -57,5 +60,32 @@ export default class InsightFacade implements IInsightFacade {
 
 	public async listDatasets(): Promise<InsightDataset[]> {
 		return Promise.reject(new InsightError());
+	}
+
+	public writeFile(id: string, content: string): Promise<any> {
+		let path = "data/" + id + ".json";
+		let data = JSON.stringify(content);
+		return new Promise((resolve, reject) => {
+			fs.writeFile(path, data, (err) => {
+				if (err) {
+					reject(err);
+				}
+				resolve("success");
+			});
+		});
+	}
+
+	public readFile(id: string): Promise<any> {
+		let path = "data/" + id + ".json";
+		return new Promise((resolve, reject) => {
+			fs.readFile(path, "utf8", (err, data: any) => {
+				if (err) {
+					reject(err);
+				} else {
+					let JsonData = JSON.parse(data);
+					resolve(JsonData);
+				}
+			});
+		});
 	}
 }
--- src/controller/Sections.ts
+++ src/controller/Sections.ts
@@ -9,16 +9,54 @@ export default class Sections {
 	private readonly pass: number;
 	private readonly fail: number;
 	private readonly audit: number;
-	constructor() {
-		this.title = "";
-		this.uuid = "";
-		this.instructor = "";
-		this.dept = "";
-		this.year = 0;
-		this.avg = 0;
-		this.id = "";
-		this.pass = 0;
-		this.fail = 0;
-		this.audit = 0;
+
+	constructor(title: string, uuid: string, instructor: string,
+		dept: string, year: number, avg: number,
+		id: string, pass: number, fail: number, audit: number) {
+		this.title = title;
+		this.uuid = uuid;
+		this.instructor = instructor;
+		this.dept = dept;
+		this.year = year;
+		this.avg = avg;
+		this.id = id;
+		this.pass = pass;
+		this.fail = fail;
+		this.audit = audit;
+	}
+
+	public update(nameIt: any): any {
+		const section: any = {};
+		if ("id" in nameIt) {
+			section["uuid"] = nameIt["id"].toString();
+		}
+		if ("Course" in nameIt) {
+			section["id"] = nameIt["Course"].toString();
+		}
+		if ("Professor" in nameIt) {
+			section["instructor"] = nameIt["Professor"].toString();
+		}
+		if ("Subject" in nameIt) {
+			section["dept"] = nameIt["Subject"].toString();
+		}
+		if ("Year" in nameIt) {
+			if (nameIt["Section"] === "overall") {
+				section["year"] = 1900;
+			} else {
+				section["year"] = Number(nameIt["Year"]);
+			}
+		}
+		if ("Avg" in nameIt) {
+			section["avg"] = nameIt["Avg"];
+		}
+		if ("Pass" in nameIt) {
+			section["pass"] = nameIt["Pass"];
+		}
+		if ("Fail" in nameIt) {
+			section["fail"] = nameIt["Fail"];
+		}
+		if ("Audit" in nameIt) {
+			section["audit"] = nameIt["Audit"];
+		}
 	}
 }
